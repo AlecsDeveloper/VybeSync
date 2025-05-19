8beafc27079b856901de04dd5536d38e693fc613
hubDEV <stevehub.dev@gmail.com>
2025-05-18 22:43:38 -0600

    PlayList System Added (Not Functional) Just Base

--- src/renderer/src/components/MainContent/MainContent.tsx
+++ src/renderer/src/components/MainContent/MainContent.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LeftSection from './panels/LeftSection'
 import CenterSection from './panels/CenterSection'
 import RightSection from './panels/RightSection'
 
+type Playlist = {
+  id: string;
+  name: string;
+  songs: string[];
+}
+
 export default function MainContent(): React.JSX.Element {
+
+  const [playlists, setPlaylists] = useState<Playlist[]>([]);
+
+  const handleCreatePlaylist = (): void => {
+    const name = prompt('Nombre de la playlist:');
+    if (name) {
+      const newPlaylist: Playlist = {
+        id: Date.now().toString(),
+        name,
+        songs: []
+      };
+
+      setPlaylists([...playlists, newPlaylist]);
+    }
+  };
+
   return (
     <section className="w-screen h-[calc(100%-124px)] flex items-center justify-center overflow-hidden">
       <div className="w-full h-full flex gap-2 px-2 overflow-hidden">
 
         <div className="w-[300px] min-w-[300px] h-full shrink-0">
-          <LeftSection />
+          <LeftSection
+            playlists={playlists}
+            onCreatePlaylist={handleCreatePlaylist}
+          />
         </div>
 
         <div className="flex-1 h-full overflow-hidden">
